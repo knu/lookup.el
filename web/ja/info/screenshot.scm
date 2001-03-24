cbe418f53da47a17ccb32193fa7c4b1e1f67b17c
knishida <knishida>
2001-03-24 22:11:01 +0000

    *** empty log message ***

--- web/ja/info/screenshot.scm
+++ web/ja/info/screenshot.scm
@@ -1,17 +1,17 @@
 (load "../header.scm")
 
-(define name "Screenshot")
 (define subtitle "スクリーンショット")
-(define date "$Date$")
+(define updated "$Date$")
+(define summary "")
 
 (define subbody
   (string-append
-   (img "jitenban97.gif")
-   (p "アスキー出版"
-      (href "『辞・典・盤97』" "http://www.ascii.co.jp/pb/jitenban/")
-      "より検索。")
-   (hr)
-   (img "aiai.jpg")
-   (p "平凡社『マイペディア99』より検索")))
+   ##(img #:src "jitenban97.gif")
+   ##(p "アスキー出版"
+	##(a #:href "http://www.ascii.co.jp/pb/jitenban/" "『辞・典・盤97』")
+	"より検索。")
+   ##(hr)
+   ##(img #:src "aiai.jpg")
+   ##(p "平凡社『マイペディア99』より検索")))
 
 (load "../subpage.scm")
