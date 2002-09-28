579801828cee662b94505913aa63a280728e245e
yamagata <yamagata>
2002-09-28 02:13:07 +0000

    (lookup-read-string): XEmacs read-from-minibuffer can't accept modified keymap and history at a time. replaced to read-string.

--- lisp/lookup-utils.el
+++ lisp/lookup-utils.el
@@ -133,30 +133,23 @@
 	(insert lookup-read-string-default)
       (backward-delete-char 1)))
 
+  (setq lookup-read-string-map (copy-keymap minibuffer-local-map))
+  (define-key lookup-read-string-map "\C-?" 'lookup-backward-char)
+  (define-key lookup-read-string-map "\C-h" 'lookup-backward-char)
+  (define-key lookup-read-string-map "\C-n" 'next-history-element)
+  (define-key lookup-read-string-map "\C-p" 'previous-history-element)
+
   (defun lookup-read-string (prompt &optional init history default inherit)
     "`read-string' に似ているが、オプション DEFAULT が指定されば場合、
 プロンプトにその値を (defaut DEFAULT) のように表示する。PROMPT には
 自動的に \": \" が付加される。"
-    (let ((lookup-read-string-default default))
-      (cond
-       ((equal ""
-	       (setq result
-		     (read-from-minibuffer 
-		      (if default
-			  (concat prompt " (default " default "): ")
-			(concat prompt ": "))
-		      init
-		      (let ((now-map (copy-keymap minibuffer-local-map)))
-			(define-key now-map  "\C-?" 'lookup-backward-char)
-			(define-key now-map  "\C-h" 'lookup-backward-char)
-			(if (fboundp 'next-history-element)
-			    (define-key now-map  "\C-n" 'next-history-element))
-			(if (fboundp 'previous-history-element)
-			    (define-key now-map  "\C-p" 'previous-history-element))
-			now-map)
-		      nil history default)))
-	default)
-       (result)))))
+    (let ((lookup-read-string-default default)
+	  (minibuffer-local-map lookup-read-string-map))
+      (read-string (if default
+		       (concat prompt " (default " default "): ")
+		     (concat prompt ": "))
+		   init history default)))
+  )
   (t
    (cond
     ((fboundp 'minibuffer-contents) ; emacs21
