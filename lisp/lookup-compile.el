4eba908ba85d0c97a66f647e816d1e70936ec068
yamagata <yamagata>
2002-09-30 19:23:34 +0000

    Newly imported from 1.99, to make elc files and lookup-autoloads.el.

--- lisp/lookup-compile.el
+++ lisp/lookup-compile.el
@@ -3,15 +3,16 @@
 (defun lookup-bytecomp ()
   (setq lookup-byte-compiling t)
   (setq load-path (nconc '("." "agent") load-path))
-  (mapc 'byte-compile-file command-line-args-left))
+  (mapcar 'load-file command-line-args-left)
+  (mapcar 'byte-compile-file command-line-args-left))
 
 (defun lookup-autoload ()
   (require 'autoload)
   (let ((generated-autoload-file "lookup-autoloads.el")
-	(make-backup-files nil))
+        (make-backup-files nil))
     (with-temp-buffer
       (setq buffer-file-name generated-autoload-file)
-      (mapc 'generate-file-autoloads command-line-args-left)
+      (mapcar 'generate-file-autoloads command-line-args-left)
       (save-buffer))))
 
 ;;; lookup-compile.el ends here
