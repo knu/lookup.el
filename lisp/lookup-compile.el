7b2143f5e80efbb33ae2bcfddf0faef294e69620
Akinori MUSHA <knu@idaemons.org>
2012-05-18 16:39:48 +0900

    Fix for Emacs 24.

--- lisp/lookup-compile.el
+++ lisp/lookup-compile.el
@@ -8,10 +8,10 @@
 
 (defun lookup-autoload ()
   (require 'autoload)
-  (let ((generated-autoload-file "lookup-autoloads.el")
-	(make-backup-files nil))
-    (with-temp-buffer
-      (mapc 'generate-file-autoloads command-line-args-left)
-      (write-file generated-autoload-file))))
+  (let ((autoload-file "lookup-autoloads.el")
+        (make-backup-files nil))
+    (with-temp-file autoload-file
+      (set-visited-file-name autoload-file)
+      (mapc 'generate-file-autoloads command-line-args-left))))
 
 ;;; lookup-compile.el ends here
